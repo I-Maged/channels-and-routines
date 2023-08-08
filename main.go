1e0b2ca4393444443d28601bcf8f6733a1e7cd6d
I-Maged <magedhanafy@gmail.com>
2023-08-08 20:46:55 +0300

    channels with for loops

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Channels and Go Routines")
+	links := []string{
+		"http://facebook.com/",
+		"http://youtube.com/",
+		"http://github.com/",
+		"http://google.com/",
+		"http://sodfughoidsfuhgsidufhg.com/"}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go connect(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+
+}
+
+func connect(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "is down")
+		c <- "Channel says link is down"
+		return
+	}
+
+	fmt.Println(link, "is connecting")
+	c <- "Channel says link is connecting"
 }
